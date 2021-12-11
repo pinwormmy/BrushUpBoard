287aa9c7aabde219ebb406c2da43c0a114673921
pinwormmy <ozkhan@naver.com>
2021-12-12 02:41:27 +0900

    어쩌다보이 글목록까지 구현해버렸는데,
    아직 받아쓰기 느낌이 강하니까 뜯어봐야됨
    이전에 기본세팅 연습

--- BrushUpBoard/src/main/java/com/brushup/Controller/HomeController.java
+++ BrushUpBoard/src/main/java/com/brushup/Controller/HomeController.java
@@ -16,6 +16,8 @@ public class HomeController {
 		System.out.println("그래서 일단 계속 프로젝트 생성 연습해서 암기해버릴거다");
 		System.out.println("그때까진 이거로 일단 커밋만 하고");
 		System.out.println("만족스러울 때 해당 프로젝트로 다시 깃허브 연동시키는거지");
+		System.out.println("db생성은 일단 무난하게 되어서..기본세팅만 연습");
+		
 		System.out.println(org.springframework.core.SpringVersion.getVersion());
 	
 		return "home";
