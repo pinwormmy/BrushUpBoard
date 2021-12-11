771a5d253d968eb7b8620a3442f91bc04db6ac6e
pinwormmy <ozkhan@naver.com>
2021-12-12 01:13:52 +0900

    계속 프로젝트생성해서 초기설정 연습
    오늘은 일단 db연동까지 테스트하는걸로

--- BrushUpBoard/src/main/java/com/brushup/Controller/HomeController.java
+++ BrushUpBoard/src/main/java/com/brushup/Controller/HomeController.java
@@ -13,6 +13,9 @@ public class HomeController {
 	public String home(Model model) {
 		
 		System.out.println("이 프로젝트 버리고 새로 쓴다 복사할거만 해서 옮기기");
+		System.out.println("그래서 일단 계속 프로젝트 생성 연습해서 암기해버릴거다");
+		System.out.println("그때까진 이거로 일단 커밋만 하고");
+		System.out.println("만족스러울 때 해당 프로젝트로 다시 깃허브 연동시키는거지");
 		System.out.println(org.springframework.core.SpringVersion.getVersion());
 	
 		return "home";
